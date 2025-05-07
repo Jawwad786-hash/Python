291b6bd96083043080321bec65f484725f760f97
Jawwad786-hash <mohdm.24.beai@acharya.ac.in>
2025-05-07 12:10:44 +0530

    Update CollatzSequence.py

--- CollatzSequence.py
+++ CollatzSequence.py
@@ -1,14 +1,15 @@
 print("Mohd Jawwad Ali Farooqui,USN:1AY24AI070,SEC:O")
 def collatz(number):
     if number % 2 == 0:
-        result = number 
+        result = number // 2
     else:
         result = 3 * number + 1
     print(result)
     return result
 try:
-    user_input = int(input("Enter an integer: "))
-    while user_input != 1:
-        user_input = collatz(user_input)
+    number = int(input("Enter number: "))
+    while number != 1:
+        number = collatz(number)
+    print(1)
 except ValueError:
     print("Please enter a valid integer.")
